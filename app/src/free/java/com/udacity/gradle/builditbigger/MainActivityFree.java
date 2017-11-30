49a94c2fb022813f946ec2cfffb06bb1b247f26b
Tanya Yuferova <tanya_yu_95@mail.ru>
2017-11-29 21:01:18 -0500

    Interstitial Ads

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFree.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFree.java
@@ -1,13 +1,59 @@
 package com.udacity.gradle.builditbigger;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
 /**
  * Created by Tanya Yuferova on 11/29/2017.
  */
 
 public class MainActivityFree extends MainActivity {
 
+    public static final String TAG = MainActivityFree.class.getName();
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initAd();
+    }
+
     @Override
     protected void setContentView() {
         setContentView(R.layout.activity_main_free);
     }
+
+    @Override
+    public void tellJoke(View view) {
+        super.tellJoke(view);
+
+        // Show ad
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.d(TAG, "The interstitial wasn't loaded yet.");
+        }
+    }
+
+    protected void initAd() {
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                // Load the next interstitial.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+    }
+
 }
